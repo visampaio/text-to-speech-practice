7be6e74c539cae5bbf34891996b4ea66bfa3ee4c
visampaio <viviane.sm3@gmail.com>
2018-03-19 20:10:48 -0400

    Add name-only reading functionality

--- function.js
+++ function.js
@@ -1,9 +1,16 @@
 const buttons = document.querySelectorAll("input[type=button]");
 let card;
+let readName = document.getElementById("readName");
 
 buttons.forEach((button) => {
-  button.addEventListener("click", () => responsiveVoice.speak(getCard(button)));
-})
+  button.addEventListener("click", function(){
+    if (readName.checked) {
+      responsiveVoice.speak(button.value);
+    } else {
+      responsiveVoice.speak(getCard(button));
+    }
+  })
+});
 
 // Compares value of input clicked to every object's name property and return content of object that match
 function getCard(e) {
