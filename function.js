9965866c6a8ddb26e305dcf36193c6068d724a60
visampaio <viviane.sm3@gmail.com>
2018-03-25 19:47:00 -0400

    add sum + repeat functionality

--- function.js
+++ function.js
@@ -1,9 +1,11 @@
 const buttons = document.querySelectorAll("input[type=button]");
 const readName = document.getElementById("readName");
 const dice = document.getElementById("dice");
+const repeat = document.getElementById("repeat");
 let nDice = document.getElementById("numberofDices");
 let nSide = document.getElementById("numberofSides")
 let diceArray = [];
+let lastSaid;
 let card;
 
 buttons.forEach((button) => {
@@ -16,7 +18,15 @@ buttons.forEach((button) => {
   })
 });
 
-dice.addEventListener("click", () => rollDice());
+dice.addEventListener("click", function(){
+  if (sumAll.checked) {
+    responsiveVoice.speak(sumDice(rollDice()).toString());
+  } else {
+    responsiveVoice.speak(rollDice().toString());
+  }
+});
+
+repeat.addEventListener("click", () => responsiveVoice.speak(lastSaid.toString()));
 
 // Compares value of input clicked to every object's name property and return content of object that match
 function getCard(e) {
@@ -28,12 +38,21 @@ function getCard(e) {
   return card.content;
 }
 
-
 function rollDice() {
   diceArray = [];
   for (i=0; i<nDice.value; i++) {
     let number = Math.floor((Math.random() * nSide.value) + 1);
     diceArray.push(number);
   }
-  responsiveVoice.speak(diceArray.toString());
+  lastSaid = diceArray.sort(function(a, b){return a-b});
+  return lastSaid;
+}
+
+function sumDice(array) {
+  let sum = 0;
+  for (i=0; i<array.length; i++) {
+    sum += array[i];
+  }
+  lastSaid = sum;
+  return lastSaid;
 }
