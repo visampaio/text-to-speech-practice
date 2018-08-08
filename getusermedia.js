cccb8b7db54629e5405bc691f4b83ef118c1bb29
visampaio <viviane.sm3@gmail.com>
2018-08-08 18:47:51 -0400

    Add extended line functionality

--- getusermedia.js
+++ getusermedia.js
@@ -55,7 +55,7 @@ var itemList = [];
         snapshot();
 
         var markers = detector.detect(imageData);
-        // drawCorners(markers);
+        drawCorners(markers);
         // drawId(markers);
         for (i = 0; i !== markers.length; ++ i) {
           itemList.push(markers[i].id);
@@ -68,31 +68,53 @@ var itemList = [];
       imageData = context.getImageData(0, 0, canvas.width, canvas.height);
     }
 
-    // function drawCorners(markers){
-    //   var corners, corner, i, j;
-    //
-    //   context.lineWidth = 3;
-    //
-    //   for (i = 0; i !== markers.length; ++ i){
-    //     corners = markers[i].corners;
-    //
-    //     context.strokeStyle = "red";
-    //     context.beginPath();
-    //
-    //     for (j = 0; j !== corners.length; ++ j){
-    //       corner = corners[j];
-    //       context.moveTo(corner.x, corner.y);
-    //       corner = corners[(j + 1) % corners.length];
-    //       context.lineTo(corner.x, corner.y);
-    //     }
-    //
-    //     context.stroke();
-    //     context.closePath();
-    //
-    //     context.strokeStyle = "green";
-    //     context.strokeRect(corners[0].x - 2, corners[0].y - 2, 4, 4);
-    //   }
-    // }
+    function drawCorners(markers){
+      var corners, corner, i, j;
+
+      context.lineWidth = 3;
+
+      for (i = 0; i !== markers.length; ++ i){
+        corners = markers[i].corners;
+
+        context.strokeStyle = "red";
+        context.beginPath();
+
+        // for (j = 0; j !== corners.length; ++ j){
+        //   corner = corners[j];
+        //   context.moveTo(corner.x, corner.y);
+        //   corner = corners[(j + 1) % corners.length];
+        //   context.lineTo(corner.x, corner.y);
+        // }
+
+        let a = corners[0].x - corners[1].x;
+        let b = corners[0].y - corners[1].y;
+        let dist = Math.sqrt( a*a + b*b );
+
+        // Formula to calculate coordinates based on angle and distance
+        // x2=x1+d×cosθ
+        // y2=y1+d×sinθ,
+
+        let cosAng = - a/dist;
+        let senAng = - b/dist;
+
+        console.log("cosseno puro", cosAng);
+        console.log((dist*2)*cosAng);
+
+        let newCorner = {};
+
+        newCorner.x = corners[0].x + (dist*2)*cosAng;
+        newCorner.y = corners[0].y + (dist*2)*senAng;
+
+        context.moveTo(corners[0].x, corners[0].y);
+        context.lineTo(newCorner.x, newCorner.y)
+
+        context.stroke();
+        context.closePath();
+
+        context.strokeStyle = "green";
+        context.strokeRect(corners[0].x - 2, corners[0].y - 2, 4, 4);
+      }
+    }
 
     // function drawId(markers){
     //   var corners, corner, x, y, i, j;
