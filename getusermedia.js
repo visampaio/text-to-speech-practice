aa63387909de0e41a092d552a670a00bcd277730
visampaio <viviane.sm3@gmail.com>
2018-08-17 16:55:11 -0400

    CONSEGUIMOS O CHROMA-KEY

--- getusermedia.js
+++ getusermedia.js
@@ -1,24 +1,40 @@
 // Sample from https://github.com/jcmellado/js-aruco
 
-var imageData, detector;
+///// Variaveis iniciadas
+var imageData, imageData2, imageData3, detector;
 var itemList = [];
-var context3, canvasOutput, video2;
+var context3, context2, canvasOutput, canvasQuadrado, video2, video3;
 
+
+///// Assim que a pagina carrega
     function onLoad(){
+
+      /// Atribuindo variaveis as tags
       video = document.getElementById("video");
       video2 = document.getElementById("video2");
+      video3 = document.getElementById("video3");
       canvas = document.getElementById("canvas");
       canvasOutput = document.getElementById("canvasOutput");
+      canvasQuadrado = document.getElementById("canvasQuadrado");
       context = canvas.getContext("2d");
       context3 = canvasOutput.getContext("2d");
+      context2 = canvasQuadrado.getContext('2d');
 
+
+      //// ?
       canvas.width = parseInt(canvas.style.width);
       canvas.height = parseInt(canvas.style.height);
+      canvasOutput.width = parseInt(canvasOutput.style.width);
+      canvasOutput.height = parseInt(canvasOutput.style.height);
+      canvasQuadrado.width = parseInt(canvasQuadrado.style.width);
+      canvasQuadrado.height = parseInt(canvasQuadrado.style.height);
 
+      /// ?
       if (navigator.mediaDevices === undefined) {
         navigator.mediaDevices = {};
       }
 
+      /// ?
       if (navigator.mediaDevices.getUserMedia === undefined) {
         navigator.mediaDevices.getUserMedia = function(constraints) {
           var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
@@ -32,16 +48,18 @@ var context3, canvasOutput, video2;
           });
         }
       }
-
+      /// ?
       navigator.mediaDevices
         .getUserMedia({ video: true })
         .then(function(stream) {
           if ("srcObject" in video) {
             video.srcObject = stream;
             video2.srcObject = stream;
+            video3.srcObject = stream;
           } else {
             video.src = window.URL.createObjectURL(stream);
             video2.src = window.URL.createObjectURL(stream);
+            video3.src = window.URL.createObjectURL(stream);
           }
         })
         .catch(function(err) {
@@ -68,23 +86,40 @@ var context3, canvasOutput, video2;
         }
       }
     }
-
+      /// ?
     function snapshot(){
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
       imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-      context3.drawImage(video2, 0, 0, canvasOutput.width, canvasOutput.height);
-      }
+      context2.drawImage(video2, 0, 0, canvasOutput.width, canvasOutput.height);
+      imageData2 = context2.getImageData(0, 0, canvasOutput.width, canvasOutput.height);
+      context3.drawImage(video3, 0, 0, canvasQuadrado.width, canvasQuadrado.height);
+      imageData3 = context3.getImageData(0, 0, canvasQuadrado.width, canvasQuadrado.height);
 
+
+      }
+      /// ?
     function drawCorners(markers){
       var corners, corner, newCorner, i, j, cornerData;
 
 
 
-      context.lineWidth = 3;
+      for (var z=0;z<imageData3.data.length;z+=4)
+      {
+        imageData3.data[z]=255-imageData3.data[z];
+        imageData3.data[z+1]=255-imageData3.data[z+1];
+        imageData3.data[z+2]=255-imageData3.data[z+2];
+        imageData3.data[z+3]=255;
+      }
+      context3.putImageData(imageData3, 0, 0);
 
+
+      context.lineWidth = 3;
+      /// ?
       for (i = 0; i !== markers.length; ++ i){
         corners = markers[i].corners;
         newCorner = corners;
+
+        context2.save();
 
         // cornerData = context.getImageData(corners[0].x, corners[0].y, 200, 200);
         // for (var z=0;z<cornerData.data.length;z+=4)
@@ -96,16 +131,10 @@ var context3, canvasOutput, video2;
         // }
         // context.putImageData(cornerData,corners[0].x, corners[0].y);
 
-
-
-        var context2 = canvas.getContext('2d');
-        var secondData = context2.createImageData(canvas.width, canvas.height);
-
-        context.save();
-
-        context2.strokeStyle = "red";
+        // context2.strokeStyle = "red";
+        // context2.globalCompositeOperation = "destination-out";
         context2.beginPath();
-
+      /// ?
 // 0 > 1
         context2.moveTo(corners[0].x, corners[0].y);
 
@@ -144,22 +173,34 @@ var context3, canvasOutput, video2;
 
 // 2 > 1
         context2.lineTo(newCorner[1].x, newCorner[1].y)
+      /// ?
 
-        context2.fill ();
+      if (markers.length > 0) {
+        // context2.clearRect(0, 0, canvasQuadrado.width, canvasQuadrado.height);
+        context2.fillStyle = "rgb(0,255,0)";
+        context2.fill();
         context2.closePath();
 
+        context2.drawImage(canvasQuadrado, 0, 0);
+      } else {
+        context2.drawImage(canvasQuadrado, 0, 0);
+      }
 
-        // for (var z=0;z<imageData.data.length;z+=4)
-        // {
-        //   secondData.data[z]=255-secondData.data[z];
-        //   secondData.data[z+1]=255-secondData.data[z+1];
-        //   secondData.data[z+2]=255-secondData.data[z+2];
-        //   secondData.data[z+3]=255;
-        // }
+      let frame = context2.getImageData(0, 0, canvasQuadrado.width, canvasQuadrado.height);
+      let l = frame.data.length / 4;
+      for (let i = 0; i < l; i++) {
+        let r = frame.data[i * 4 + 0];
+        let g = frame.data[i * 4 + 1];
+        let b = frame.data[i * 4 + 2];
+        if (g > 254 && r < 1 && b < 1)
+          frame.data[i * 4 + 3] = 0;
+      }
+      context2.putImageData(frame, 0, 0);
 
-        context2.drawImage(canvas, 0, 0);
 
-        context2.globalCompositeOperation = "copy";
+
+      /// ?
+        // context2.globalCompositeOperation = "copy";
 
 
         // context.strokeStyle = "green";
