7f12fd73bbb2916c0e9466902e081caab0a45375
visampaio <viviane.sm3@gmail.com>
2018-08-16 20:44:06 -0400

    dois vídeos diferentes um do lado do outro

--- getusermedia.js
+++ getusermedia.js
@@ -1,12 +1,16 @@
 // Sample from https://github.com/jcmellado/js-aruco
 
-var video, canvas, context, imageData, detector;
+var imageData, detector;
 var itemList = [];
+var context3, canvasOutput, video2;
 
     function onLoad(){
       video = document.getElementById("video");
+      video2 = document.getElementById("video2");
       canvas = document.getElementById("canvas");
+      canvasOutput = document.getElementById("canvasOutput");
       context = canvas.getContext("2d");
+      context3 = canvasOutput.getContext("2d");
 
       canvas.width = parseInt(canvas.style.width);
       canvas.height = parseInt(canvas.style.height);
@@ -34,8 +38,10 @@ var itemList = [];
         .then(function(stream) {
           if ("srcObject" in video) {
             video.srcObject = stream;
+            video2.srcObject = stream;
           } else {
             video.src = window.URL.createObjectURL(stream);
+            video2.src = window.URL.createObjectURL(stream);
           }
         })
         .catch(function(err) {
@@ -66,10 +72,13 @@ var itemList = [];
     function snapshot(){
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
       imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-    }
+      context3.drawImage(video2, 0, 0, canvasOutput.width, canvasOutput.height);
+      }
 
     function drawCorners(markers){
-      var corners, corner, newCorner, i, j;
+      var corners, corner, newCorner, i, j, cornerData;
+
+
 
       context.lineWidth = 3;
 
@@ -77,11 +86,28 @@ var itemList = [];
         corners = markers[i].corners;
         newCorner = corners;
 
-        context.strokeStyle = "red";
-        context.beginPath();
+        // cornerData = context.getImageData(corners[0].x, corners[0].y, 200, 200);
+        // for (var z=0;z<cornerData.data.length;z+=4)
+        // {
+        //   cornerData.data[z]=255-cornerData.data[z];
+        //   cornerData.data[z+1]=255-cornerData.data[z+1];
+        //   cornerData.data[z+2]=255-cornerData.data[z+2];
+        //   cornerData.data[z+3]=255;
+        // }
+        // context.putImageData(cornerData,corners[0].x, corners[0].y);
+
+
+
+        var context2 = canvas.getContext('2d');
+        var secondData = context2.createImageData(canvas.width, canvas.height);
+
+        context.save();
+
+        context2.strokeStyle = "red";
+        context2.beginPath();
 
 // 0 > 1
-        context.moveTo(corners[0].x, corners[0].y);
+        context2.moveTo(corners[0].x, corners[0].y);
 
         let a = corners[0].x - corners[1].x;
         let b = corners[0].y - corners[1].y;
@@ -93,10 +119,10 @@ var itemList = [];
         newCorner[1].x = corners[0].x + (distHor*2)*cosHor;
         newCorner[1].y = corners[0].y + (distHor*2)*senHor;
 
-        context.lineTo(newCorner[1].x, newCorner[1].y)
+        context2.lineTo(newCorner[1].x, newCorner[1].y)
 
 // 0 > 3
-        context.moveTo(corners[0].x, corners[0].y);
+        context2.moveTo(corners[0].x, corners[0].y);
 
         a = corners[0].x - corners[3].x;
         b = corners[0].y - corners[3].y;
@@ -108,22 +134,36 @@ var itemList = [];
         newCorner[3].x = corners[0].x + (distVer*2)*cosVer;
         newCorner[3].y = corners[0].y + (distVer*2)*senVer;
 
-        context.lineTo(newCorner[3].x, newCorner[3].y)
+        context2.lineTo(newCorner[3].x, newCorner[3].y)
 
 // 3 > 2
         newCorner[2].x = newCorner[3].x + (distHor*2)*cosHor;
         newCorner[2].y = newCorner[3].y + (distHor*2)*senHor;
 
-        context.lineTo(newCorner[2].x, newCorner[2].y)
+        context2.lineTo(newCorner[2].x, newCorner[2].y)
 
 // 2 > 1
-        context.lineTo(newCorner[1].x, newCorner[1].y)
+        context2.lineTo(newCorner[1].x, newCorner[1].y)
 
-        context.stroke();
-        context.closePath();
+        context2.fill ();
+        context2.closePath();
 
-        context.strokeStyle = "green";
-        context.strokeRect(corners[0].x - 2, corners[0].y - 2, 4, 4);
+
+        // for (var z=0;z<imageData.data.length;z+=4)
+        // {
+        //   secondData.data[z]=255-secondData.data[z];
+        //   secondData.data[z+1]=255-secondData.data[z+1];
+        //   secondData.data[z+2]=255-secondData.data[z+2];
+        //   secondData.data[z+3]=255;
+        // }
+
+        context2.drawImage(canvas, 0, 0);
+
+        context2.globalCompositeOperation = "copy";
+
+
+        // context.strokeStyle = "green";
+        // context.strokeRect(corners[0].x - 2, corners[0].y - 2, 4, 4);
       }
     }
 
